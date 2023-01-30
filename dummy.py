156a7dfc8a92f10d8b93f14b9e895f8cd5764020
huonglarne <nguyenlanhuong.vietnam@gmail.com>
2023-01-30 17:18:24 +0900

    minimal run

--- dummy.py
+++ dummy.py
@@ -1,3 +1,23 @@
-from whisper.transcribe import cli
+# from whisper.transcribe import cli
 
-cli()
+# cli()
+
+from typing import Optional
+import torch
+from torch import Tensor, nn
+import torch.nn.functional as F
+
+class Conv1d(nn.Conv1d):
+    def _conv_forward(self, x: Tensor, weight: Tensor, bias: Optional[Tensor]) -> Tensor:
+        weight = weight.to(x.dtype)
+        return super()._conv_forward(
+            x, weight, None if bias is None else bias.to(x.dtype)
+        )
+
+conv = Conv1d(80, 384, kernel_size=3, padding=1)
+conv = conv.cuda()
+
+x = torch.rand(1, 80, 3000, dtype=torch.float16)
+x = x.cuda()
+
+conv(x)
